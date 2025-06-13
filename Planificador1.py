d28fb4eca9ce9d560fa3d021bf032782da41afd2
camiparra-byte <cparra@itba.edu.ar>
2025-06-12 23:00:01 -0300

    Update Planificador1.py

--- Planificador1.py
+++ Planificador1.py
@@ -1,5 +1,5 @@
 import math
-from conexion_nodos_solicitud import Conexiones
+from conexion_nodos_solicitud import Conexiones, Nodos
 from TPFINAL import Camion, Tren, Barco, Avion
 
 import math
@@ -16,77 +16,42 @@ class Planificador:
         if rutas_encontradas is None:
             rutas_encontradas = []
 
+        nodos_visitados.add(origen)
+
         if origen == destino:
             rutas_encontradas.append(list(ruta_actual))
-            return
-
-        for conexion in Conexiones.Conexiones_existentes.get(origen, []):
-            if conexion.tipo.lower() != tipo_transporte.lower():
-                continue
-
-            siguiente = conexion.nodo_destino
-            if siguiente in nodos_visitados:
-                continue
-
-            ruta_actual.append(conexion)
-            nodos_visitados.add(siguiente)
-
-            Planificador.encontrar_rutas(siguiente, destino, tipo_transporte, ruta_actual, nodos_visitados, rutas_encontradas)
-
-            ruta_actual.pop()
-            nodos_visitados.remove(siguiente)
-
-    @staticmethod
-    def evaluar_rutas(origen, destino, peso_carga, vehiculos_disponibles):
-        resultados = []
-
-        for vehiculo in vehiculos_disponibles:
-            rutas_encontradas = []
-            Planificador.encontrar_rutas(origen, destino, vehiculo.nombre.lower(), [], set([origen]), rutas_encontradas)
-
-            for ruta in rutas_encontradas:
-                tipos_ok = True
-                for conexion in ruta:
-                    if conexion.tipo.lower() not in vehiculo.nombre.lower():
-                        tipos_ok = False
-                    if conexion.restriccion == "velocidad maxima" and vehiculo.velocidad > conexion.valor_de_restriccion:
-                        tipos_ok = False
-                    if conexion.restriccion == "peso maximo" and peso_carga > conexion.valor_de_restriccion:
-                        tipos_ok = False
-                if not tipos_ok:
-                    continue
-
-                cantidad = math.ceil(peso_carga / vehiculo.capacidad)
-                distancia_total = sum(conexion.distancia for conexion in ruta)
-
-                if isinstance(vehiculo, Camion):
-                    costo = vehiculo.calcular_costo(distancia_total, peso_carga)
-                    tiempo = distancia_total / vehiculo.velocidad
-
-                elif isinstance(vehiculo, Tren):
-                    costo = cantidad * vehiculo.calcular_costo(distancia_total, peso_carga)
-                    tiempo = distancia_total / vehiculo.velocidad
-
-                elif isinstance(vehiculo, Barco):
-                    tipo = ruta[0].tipo.lower()
-                    costo = cantidad * vehiculo.calcular_costo(distancia_total, peso_carga, tipo=tipo)
-                    tiempo = distancia_total / vehiculo.velocidad
-
-                elif isinstance(vehiculo, Avion):
-                    tiempo = vehiculo.calcular_tiempo(distancia_total, prob_mal_tiempo=0.3)
-                    costo = cantidad * vehiculo.calcular_costo(distancia_total, peso_carga)
-
-                resultados.append({
-                    "ruta": ruta,
-                    "vehiculo": vehiculo.nombre,
-                    "costo": costo,
-                    "tiempo": tiempo,
-                    "cantidad_vehiculos": cantidad
-                })
-
-        if resultados:
-            mas_barata = min(resultados, key=lambda x: x["costo"])
-            mas_rapida = min(resultados, key=lambda x: x["tiempo"])
-            return mas_barata, mas_rapida
         else:
-            return None, None
+            for nodo_vecino, conexiones in Nodos.nodos_existentes[origen].conexiones.items():
+                for conexion in conexiones:
+                    if conexion.tipo.lower() != tipo_transporte.lower():
+                        continue
+                    if nodo_vecino in nodos_visitados:
+                        continue
+
+                    ruta_actual.append(conexion)
+
+                    Planificador.encontrar_rutas(nodo_vecino, destino, tipo_transporte, ruta_actual, nodos_visitados, rutas_encontradas)
+
+                    ruta_actual.pop()
+
+        nodos_visitados.remove(origen)
+
+        return rutas_encontradas
+    
+#Zarate,Junin,Automotor,185,peso_max,15000
+    @staticmethod
+    def evaluar_rutas_automotor(origen, destino, peso_carga):
+            resultados = []
+
+            camion = Camion()
+
+            rutas = Planificador.encontrar_rutas(origen, destino, "automotor")
+
+            peso_max = camion.capacidad
+
+            for ruta in rutas:
+                for conexion in ruta:
+                    if conexion.restriccion == "peso maximo":
+                        peso_max = min(peso_max, conexion.valor_de_restriccion)
+
+                #continuar
