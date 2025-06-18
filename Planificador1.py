327f12862cb76965dc593d3a36f10a05a9fb952b
camiparra-byte <cparra@itba.edu.ar>
2025-06-18 20:43:51 -0300

    Update Planificador1.py

--- Planificador1.py
+++ Planificador1.py
@@ -1,4 +1,4 @@
-import math
+ import math
 from conexion_nodos_solicitud import Conexiones, Nodos
 from TPFINAL import Camion, Tren, Barco, Avion
 
@@ -7,7 +7,7 @@ from conexion_nodos_solicitud import Conexiones
 from TPFINAL import Camion, Tren, Barco, Avion
 
 
-class Planificador:
+class Planificador:  
     @staticmethod
     def encontrar_rutas(origen, destino, tipo_transporte, ruta_actual=None, nodos_visitados=None, rutas_encontradas=None):
         if ruta_actual is None:
@@ -75,10 +75,86 @@ class Planificador:
 
         
         mejor_por_costo = min(resultados, key=lambda r: r["costo_total"])
-        mejor_por_tiempo = min(resultados, key=lambda r: r["distancia_total"])
+        mejor_por_tiempo = min(resultados, key=lambda r: r["distancia_total"]) #falta dividirlo por la velocidad 
 
         return {"mejor_por_costo":mejor_por_costo,"mejor_por_tiempo": mejor_por_tiempo }  #capaz me conviene ponerlo en una lista o similar para cuando comparo con los otros medios despues
-        
+                                                                                            #Ademas aca estamos obteniendo unicamente el mejor costo y el mejor tiempo pero no las rutas enteras que tienen ese costo y ese tiempo
+
+    
+    
+    @staticmethod
+    def evaluar_rutas_maritimo(origen, destino, peso_carga):
+        resultados = []
+
+        barco = Barco()
+        rutas = Planificador.encontrar_rutas(origen, destino, "maritimo")
+        cantidad=math.ceil(peso_carga/barco.capacidad)
+        for ruta in rutas:
+            distancia_total= 0
+            costo_ruta=0
+            for conexion in ruta:
+                distancia_total += conexion.distancia
+                costo_ruta+=barco.calcular_costo(conexion.distancia,peso_carga,conexion.valor_de_restriccion)
+            tiempo=distancia_total/barco.get_velocidad  #hay que hacer geters de esto para obtener
+            resultados.append({"ruta":ruta ,"costo": costo_ruta * cantidad , "tiempo":tiempo})
 
             
+        mejor_por_costo = min(resultados, key=lambda r: r["costo_total"])
+        mejor_por_tiempo = min(resultados, key=lambda r: r["distancia_total"])
 
+        return {
+            "mejor_por_costo": mejor_por_costo,
+            "mejor_por_tiempo": mejor_por_tiempo
+        }
+
+    @staticmethod
+    def evaluar_rutas_ferroviario(origen, destino, peso_carga):
+        resultados = []
+
+        tren = Tren()
+        rutas = Planificador.encontrar_rutas(origen, destino, "ferroviario")
+        cantidad=math.ceil(peso_carga/tren.capacidad)
+        for ruta in rutas:
+            distancia_total = 0
+            tiempo_total = 0
+            
+            for conexion in ruta:
+                distancia_total += conexion.distancia
+                if conexion.restriccion != None:     #es necesario este if?
+                    velocidad= min(conexion.valor_de_restriccion,tren.get_velocidad)
+                    tiempo_total+= (conexion.distancia/velocidad)
+                else:
+                    tiempo_total += (conexion.distancia / tren.get_velocidad)
+
+            costo_ruta= tren.calcular_costo(distancia_total,peso_carga)
+            resultados.append({"ruta":ruta, "cantidad":cantidad, "costo":costo_ruta *cantidad, "tiempo":tiempo_total})
+        
+
+        mejor_por_costo = min(resultados, key=lambda r: r["costo_total"])
+        mejor_por_tiempo = min(resultados, key=lambda r: r["tiempo_total"])
+
+        return {
+            "mejor_por_costo": mejor_por_costo,
+            "mejor_por_tiempo": mejor_por_tiempo
+        }
+
+    @staticmethod
+    def evaluar_rutas_aerea(origen,destino,peso_carga):
+        resultados=[]
+
+        avion=Avion()
+        rutas=Planificador.encontrar_rutas(origen,destino,"aereo")
+        cantidad = math.ceil(peso_carga/avion.capacidad)
+        for ruta in rutas:
+            distancia_total=0
+            tiempo_total=0
+            
+            for conexion in ruta:
+                distancia_total += conexion.distancia
+                velocidad= avion.get_velocidad(conexion.prob_mal_tiempo)
+                tiempo_total += (conexion.distancia/velocidad)
+            
+            costo_total= avion.calcular_costo(distancia_total, peso_carga)
+            resultados.append({"ruta":ruta,"cantidad":cantidad, "costo":costo_total *cantidad, "tiempo":tiempo_total})
+        
+            
