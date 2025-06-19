913a013cf476e9c33788eeeb0110244f834dd480
PiaBarchigia <pbarchigia@itba.edu.ar>
2025-06-19 01:00:09 -0300

    Update por meet main.py

--- main.py
+++ main.py
@@ -11,78 +11,32 @@ lector.leer_csv("conexiones.csv", "conexion")
 lector.leer_csv("solicitudes.csv", "solicitud")
 
 
-# print("Nodos existentes:")
+#print("Nodos existentes:")
 # for nombre, nodo in Nodos.nodos_existentes.items():
 #     print(nodo)
 #     print("-" * 40)
 
 
-rutas,nodos = Planificador.encontrar_rutas("Zarate", "Mar_del_Plata", "Ferroviaria")
+print(Solicitud.solicitudes_existentes)
 
-print("Rutas encontradas Ferroviaria:")
-for ruta in rutas:
-    print("Ruta:")
-    print(ruta)
-
-rutas = Planificador.encontrar_rutas("Zarate", "Mar_del_Plata", "Automotor")
-
-print("Rutas encontradas Automotor:")
-for ruta in rutas:
-    print("Ruta:")
-    print(ruta)
-
-rutas = Planificador.encontrar_rutas("Zarate", "Mar_del_Plata", "Fluvial")
-
-print("Rutas encontradas Fluvial:")
-for ruta in rutas:
-    print("Ruta:")
-    print(ruta)
-
-rutas = Planificador.encontrar_rutas("Zarate", "Mar_del_Plata", "Aerea")
-
-print("Rutas encontradas Aerea:")
-for ruta in rutas:
-    print("Ruta:")
-    print(ruta)
-
-
-print("\nSolicitudes existentes:")
 for solicitud in Solicitud.solicitudes_existentes.values():
-    print(solicitud)
+    print(Planificador.evaluar_mejores_rutas(solicitud.origen,solicitud.destino,solicitud.peso))
+
+
+
+
+#   automotor=Planificador.evaluar_rutas_automotor(solicitud.origen,solicitud.destino,solicitud.peso)
+#   maritimo=Planificador.evaluar_rutas_maritimo(solicitud.origen,solicitud.destino,solicitud.peso)
+#   ferroviaria=Planificador.evaluar_rutas_ferroviario(solicitud.origen,solicitud.destino,solicitud.peso)
+#   aerea=Planificador.evaluar_rutas_aerea(solicitud.origen,solicitud.destino,solicitud.peso)
+  
+
+#   print(automotor)
+#   print(maritimo)
+#   print(ferroviaria)
+#   print(aerea)
     
-from Graficos import Itinerario
-from TPFINAL import Camion, Tren , Barco, Avion  
 
-vehiculos_por_tipo = {
-    "Automotor": Camion,
-    "Ferroviaria": Tren,
-    "Fluvial": Barco,
-    "Aerea": Avion
-}
-
-print("\n--- Generación de gráfico para la primera solicitud con ruta válida ---")
-
-graficado = False  # bandera para cortar la iteración 
-
-for solicitud in Solicitud.solicitudes_existentes.values():
-    if graficado:
-        continue
-
-    origen = solicitud.origen
-    destino = solicitud.destino
-    tipo = solicitud.tipo_transporte
-    peso = solicitud.peso_kg
-
-    rutas, _ = Planificador.encontrar_rutas(origen, destino, tipo)
-
-    if rutas:
-        clase_vehiculo = vehiculos_por_tipo.get(tipo)
-        if clase_vehiculo:
-            vehiculo = clase_vehiculo()
-            ruta_elegida = rutas[0]
-            itinerario = Itinerario(ruta_elegida)
-            itinerario.graficar(vehiculo, peso)
-            graficado = True
 
 
 
