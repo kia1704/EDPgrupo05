781e9ef42fec88b29e548f4e3755b019a0db929e
afranco5 <afranco@itba.edu.ar>
2025-06-19 17:22:01 -0300

    DEF

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from conexion_nodos_solicitud import Nodos
-from conexion_nodos_solicitud import Conexiones
+#from conexion_nodos_solicitud import Nodos
+#from conexion_nodos_solicitud import Conexiones
 from LectorCSV2 import LectorCSV2
-
 from conexion_nodos_solicitud import Solicitud
 from Planificador1 import Planificador
+from graficos import GraficadorRuta
+
 
 lector = LectorCSV2()
 lector.leer_csv("nodos.csv", "nodo")
@@ -11,31 +12,19 @@ lector.leer_csv("conexiones.csv", "conexion")
 lector.leer_csv("solicitudes.csv", "solicitud")
 
 
-#print("Nodos existentes:")
-# for nombre, nodo in Nodos.nodos_existentes.items():
-#     print(nodo)
-#     print("-" * 40)
-
-
-print(Solicitud.solicitudes_existentes)
 
 for solicitud in Solicitud.solicitudes_existentes.values():
-    print(Planificador.evaluar_mejores_rutas(solicitud.origen,solicitud.destino,solicitud.peso))
-
-
-
-
-#   automotor=Planificador.evaluar_rutas_automotor(solicitud.origen,solicitud.destino,solicitud.peso)
-#   maritimo=Planificador.evaluar_rutas_maritimo(solicitud.origen,solicitud.destino,solicitud.peso)
-#   ferroviaria=Planificador.evaluar_rutas_ferroviario(solicitud.origen,solicitud.destino,solicitud.peso)
-#   aerea=Planificador.evaluar_rutas_aerea(solicitud.origen,solicitud.destino,solicitud.peso)
-  
-
-#   print(automotor)
-#   print(maritimo)
-#   print(ferroviaria)
-#   print(aerea)
     
+    resultado=Planificador.evaluar_mejores_rutas(solicitud.origen,solicitud.destino,solicitud.peso)
+    print(resultado)
+    mejor_ruta = resultado["Mejor ruta por costo (tipo, info)"]  # o por tiempo
+
+    if mejor_ruta:
+        ruta_info = mejor_ruta[1] 
+        graficador = GraficadorRuta(ruta_info)
+        graficador.graficar()
+    else:
+        print("No se encontró una ruta óptima para graficar.")
 
 
 
