b8292d2ca58e5285c7c1588f90a42c2525a3b40b
kia1704 <ccampolungo@itba.edu.ar>
2025-07-05 22:38:08 -0300

    Update Graficos.py

--- Graficos.py
+++ Graficos.py
@@ -10,20 +10,13 @@ def graficar_puntos_tramos(puntos, titulo, xlabel, ylabel, color='blue', marker=
     color: color de la curva
     marker: marcador de los puntos
     """
-    distancias = [0]
-    valores = [0]
-    suma_dist = 0
-    suma_valor = 0
-    for d, v in puntos:
-        suma_dist += d
-        suma_valor += v
-        distancias.append(suma_dist)
-        valores.append(suma_valor)
-
-    plt.figure(figsize=(10, 5))
+    from itertools import accumulate
+    distancias = [0] + list(accumulate(map(lambda p: p[0], puntos)))
+    valores = [0] + list(accumulate(map(lambda p: p[1], puntos)))
+    
     plt.plot(distancias, valores, marker=marker, color=color)
     for x, y in zip(distancias[1:], valores[1:]):
-        plt.annotate(f"({x:.0f}, {y:.0f})", (x, y), textcoords="offset points", xytext=(0,10), ha='center', fontsize=9)
+        plt.annotate(f'({x:.0f}, {y:.0f})', (x, y), textcoords='offset points', xytext=(0, 10), ha='center', fontsize=9)
     plt.title(titulo)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
