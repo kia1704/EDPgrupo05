fee0cb07b1260427a9fcb66140d9c095479ef6c0
kia1704 <ccampolungo@itba.edu.ar>
2025-07-05 22:44:04 -0300

    Update Planificador1.py

--- Planificador1.py
+++ Planificador1.py
@@ -2,9 +2,8 @@ import math
 from conexion_nodos_solicitud import Conexiones, Nodos
 from TPFINAL import Camion, Tren, Barco, Avion
 
+class Planificador:
 
-
-class Planificador:  
     @staticmethod
     def validar_nodo(nombre):
         if nombre not in Nodos.nodos_existentes:
@@ -12,100 +11,72 @@ class Planificador:
 
     @staticmethod
     def validar_tipo_transporte(tipo):
-        if tipo.lower() not in ["automotor", "ferroviaria", "fluvial", "aerea"]:
+        if tipo.lower() not in ['automotor', 'ferroviaria', 'fluvial', 'aerea']:
             raise ValueError(f"Tipo de transporte '{tipo}' no válido.")
 
     @staticmethod
     def validar_peso(peso):
         if peso <= 0:
-            raise ValueError("El peso de carga debe ser positivo.")
-        
+            raise ValueError('El peso de carga debe ser positivo.')
+
     @staticmethod
     def encontrar_rutas(origen, destino, ruta_actual=None, nodos_visitados=None, rutas_encontradas=None):
         Planificador.validar_nodo(origen)
         Planificador.validar_nodo(destino)
-        #Planificador.validar_tipo_transporte(tipo_transporte)
-
         if ruta_actual is None:
             ruta_actual = []
         if nodos_visitados is None:
             nodos_visitados = set()
         if rutas_encontradas is None:
             rutas_encontradas = []
-
         nodos_visitados.add(origen)
-
         if origen == destino:
             rutas_encontradas.append(list(ruta_actual))
         else:
             for nodo_vecino, conexiones in Nodos.nodos_existentes[origen].conexiones.items():
                 for conexion in conexiones:
-                # for conexion in filter(lambda conexion: conexion.tipo.lower() == tipo_transporte.lower(), conexiones):
                     if nodo_vecino in nodos_visitados:
                         continue
                     if not isinstance(conexion, Conexiones):
-                        raise TypeError("Cada tramo de la ruta debe ser una instancia de Conexiones.")
-                    
+                        raise TypeError('Cada tramo de la ruta debe ser una instancia de Conexiones.')
                     ruta_actual.append(conexion)
-
                     Planificador.encontrar_rutas(nodo_vecino, destino, ruta_actual, nodos_visitados, rutas_encontradas)
-
                     ruta_actual.pop()
-
         nodos_visitados.remove(origen)
-
         return rutas_encontradas
 
     @staticmethod
-    def evaluar_rutas(origen,destino,peso_carga):
-        resultados=[]
-        rutas= Planificador.encontrar_rutas(origen,destino)
-
+    def evaluar_rutas(origen, destino, peso_carga):
+        resultados = []
+        rutas = Planificador.encontrar_rutas(origen, destino)
         for ruta in rutas:
-            
             distancia_total = 0
-            
             graf_c = []
             graf_t = []
-            costo_ruta=0
-            tiempo_ruta=0
-            tipo=ruta[0].get_tipo()
-            c_pesos=[]
+            costo_ruta = 0
+            tiempo_ruta = 0
+            tipo = ruta[0].get_tipo()
+            c_pesos = []
             costo_tras = 0
             for conexion in ruta:
-                
                 if conexion.get_tipo() != tipo:
                     costo_tras += conexion.get_nodo_origen().trasbordo(peso_carga)
-                
-                tipo= conexion.get_tipo()
-                
+                tipo = conexion.get_tipo()
                 distancia_total += conexion.get_distancia()
-                costo_tramo= conexion.calcular_costo(peso_carga)
-                tiempo_tramo= conexion.calcular_tiempo()
-                costo_ruta+=costo_tramo
-                tiempo_ruta+= tiempo_tramo
+                costo_tramo = conexion.calcular_costo(peso_carga)
+                tiempo_tramo = conexion.calcular_tiempo()
+                costo_ruta += costo_tramo
+                tiempo_ruta += tiempo_tramo
                 graf_c.append((conexion.get_distancia(), costo_tramo))
                 graf_t.append((conexion.get_distancia(), tiempo_tramo))
-            
-            resultados.append({
-                "ruta": ruta,
-                "costo": costo_ruta + costo_tras,
-                "tiempo": tiempo_ruta,
-                "distancia vs tiempo": graf_t,
-                "distancia vs costo": graf_c
-            })
-
+            resultados.append({'ruta': ruta, 'costo': costo_ruta + costo_tras, 'tiempo': tiempo_ruta, 'distancia vs tiempo': graf_t, 'distancia vs costo': graf_c})
         if resultados:
-            mejor_por_costo = min(resultados, key=lambda r: r["costo"])
-            mejor_por_tiempo = min(resultados, key=lambda r: r["tiempo"])
+            mejor_por_costo = min(resultados, key=lambda r: r['costo'])
+            mejor_por_tiempo = min(resultados, key=lambda r: r['tiempo'])
         else:
             mejor_por_costo = None
             mejor_por_tiempo = None
-
-        return {
-            "Mejor costo": mejor_por_costo,
-            "Mejor tiempo": mejor_por_tiempo
-        }
+        return {'Mejor costo': mejor_por_costo, 'Mejor tiempo': mejor_por_tiempo}
 
     # @staticmethod
     # def evaluar_rutas_automotor(origen, destino, peso_carga):
